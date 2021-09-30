c9f06d1633e043e6e09f34fc34b80d686092b1d0
bartusbiborka <91243585+bartusbiborka@users.noreply.github.com>
2021-09-30 07:37:15 +0300

    matrix random elemek
    Hozzunk létre egy NxN es mátrixot, amelyet véletlenszámokkal (random) töltönk fel, majd a főátlón lévő elemeket nullázzuk le, az alatta levőek pedig legyenek negatívak (v[i][j] = -v[i][j]). Az így kapott mátrixot írassuk ki a képernyőre!

---
+++ matrix_foatlo0_negativ/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+   int i,j,m[100][100], n;
+   cout << "n=";
+   cin >> n;
+   for (i=0; i<n; i++){
+    for (j=0; j<n; j++){
+        m[i][j]= rand()%100;
+    }
+   }
+   for (i=0; i<n; i++){
+        m[i][i]=0;
+         for (j=0; j<n; j++){
+            if (i>j){
+                m[i][j] = -m[i][j];
+   }
+    }
+   }
+    for (i=0; i<n; i++){
+    for (j=0; j<n; j++){
+        cout << m[i][j] << " ";
+    } cout << endl;}
+
+    return 0;
+}
