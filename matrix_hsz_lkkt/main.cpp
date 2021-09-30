cdb5c66d79b8ca9692fef91a385f6d5c2fe66213
bartusbiborka <91243585+bartusbiborka@users.noreply.github.com>
2021-09-30 09:24:20 +0300

    matrix lkkt
     Hozzunk létre egy NxN es mátrixot és töltsük fel egy "input.txt" fileból, aztán számítsuk ki a főátló feletti és alatti elemek összegét. Az így kapott két számnak a legkiseb közös többszörösét írassuk ki egy "lkkt.out" fileba.

---
+++ matrix_hsz_lkkt/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("lkkt.txt");
+    int v[100][100], n, f=0, a=0;
+    in >> n;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            in >> v[i][j];
+        }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j <n; j++){
+            if (i<j){
+                f += v[i][j];
+            }
+            if (i>j){
+                a += v[i][j];
+            }
+        }
+            }
+    int x, lkkt=0;
+    x=f*a;
+    while (a!=f){
+    if (f>a){
+        f=f-a;
+    }else {
+    a=a-f;}}
+    lkkt = x/a;
+    out << "lkkt: " << lkkt;
+    in.close();
+    out.close();
+    return 0;
+}
