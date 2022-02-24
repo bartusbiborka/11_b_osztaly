ebaeca538cf6d79a79266fcf136b60067f39421c
bartusbiborka <91243585+bartusbiborka@users.noreply.github.com>
2022-02-24 09:27:21 +0200

    Add files via upload

---
+++ backtracking_ital/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N = 5;
+int szamlalo=2;
+string virag[] = {"afonya", "kajszin", "citrom", "alma", "korte" };
+
+bool lehet(vector<string> ruhak, string ujruha)
+{
+    for(int i = 0; i<ruhak.size(); i++)
+    {
+        if (ruhak[i] == ujruha)
+        {
+            return false;
+        }
+    }
+    if (ujruha == "citrom")
+    {
+        for(int i = 0; i<ruhak.size(); i++)
+        {
+
+            if (ruhak[i] == "korte")
+            {
+                return false;
+            }
+        }
+
+
+    }
+    if (ujruha == "korte")
+    {
+        for(int i = 0; i<ruhak.size(); i++)
+        {
+            if (ruhak[i] == "citrom")
+            {
+               return false;
+            }
+
+        }
+
+    }
+    if(ujruha == "alma" && ruhak[0]=="afonya"){
+        return false;
+    }
+    return true;
+}
+void kiir(vector<string> csokor)
+{
+    for(int i = 0; i<csokor.size(); i++)
+    {
+        cout << csokor[i] << " " ;
+    }
+    cout << endl;
+    return;
+}
+void backtrack(vector<string> koktel, int index)
+{
+
+    if (koktel.size() < 5 && koktel.size() >1)
+    {
+        if(koktel[0] == "afonya" && koktel[koktel.size()-1]=="alma"){
+            return;
+        }else{
+        kiir(koktel);}
+
+    }
+    for(int i = 0; i<5; i++)
+    {
+        if (lehet(koktel, virag[i]))
+        {
+
+            vector<string> tempcsokor;
+            for(int j = 0; j<koktel.size(); j++)
+            {
+                tempcsokor.push_back(koktel[j]);
+            }
+            tempcsokor.push_back(virag[i]);
+            backtrack(tempcsokor, i);
+        }
+    }
+
+
+}
+int main()
+{
+    vector<string> koktel;
+
+    backtrack(koktel, 0);
+    return 0;
+}
