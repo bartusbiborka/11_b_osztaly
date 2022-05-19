7e812bd11d72e73dbe1e79150b78349da5ccd412
bartusbiborka <91243585+bartusbiborka@users.noreply.github.com>
2022-05-19 09:35:59 +0300

    8asra
    rjon egy C/C++ programot amely beolvas a billentyűzetről egy n (n∈[2,20]) természetes számot és
    felépít a memóriában egy olyan kétdimenziós tömböt, amelynek n sora és n oszlopa van úgy, hogy
    minden sor a következő jellemzőkkel rendelkezzen:
    • a mellékátlón levő elem értéke n;
    • a mellékátlótól balra haladva és a mellékátlótól jobbra haladva is az elemek egymásutáni értékek,
    szigorúan csökkenő sorozatot alkotva!
    A program írja ki a képernyőre a felépített tömböt úgy, hogy minden sorát külön sorba
    írja, és egy soron belül az elemek szóközzel legyenek elválasztva!
    Példa: ha n=5 a mellékelt tömb legyen kiírva. (10p.)
    1 2 3 4 5
    2 3 4 5 4
    3 4 5 4 3
    4 5 4 3 2
    5 4 3 2 1

---
+++ 2022_05_19/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int v[100][100];
+    cout << "n=";
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(j==0)
+            {
+                v[i][j]=i+1;
+            }
+            else if(i+j==n-1)
+            {
+                v[i][j]=n;
+            }
+            else if(i+j <n-1)
+            {
+                v[i][j]=v[i][j-1]+1;
+            }
+            else
+            {
+                v[i][j]=v[i][j-1] -1;
+            }
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
