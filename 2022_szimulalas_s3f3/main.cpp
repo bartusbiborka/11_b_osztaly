df8c0cacd0d5b9e16e78094443327c720dd2ecc7
bartusbiborka <91243585+bartusbiborka@users.noreply.github.com>
2023-02-10 07:25:42 +0200

    Add files via upload
    A billentyűzetről beolvasunk egy n (n[1,109]) természetes számot, és a bac.txt szöveges
    állományba ki kell íratni azt a legnagyobb p természetes számot, amelyre 45p osztója a 1∙2∙3∙...∙n
    szorzat kiértékelése során kapott számnak.
    Tervezzen a futási idő és a felhasznált memória szempontjából hatékony algoritmust.
    Példa: ha n=14, az állomány tartalma 2 lesz (452=2025 osztója az 1∙2∙3∙..∙14=87178291200 számnak

---
+++ 2022_szimulalas_s3f3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream out("bac.txt");
+    int n, k3=0, k5=0;
+    cout << "n=";
+    cin >> n;
+    for(int i=1; i<n; i++)
+    {
+        int j=i;
+        while(j%3==0)
+        {
+            k3++;
+            j/=3;
+        }
+        while(j%5==0)
+        {
+            k5++;
+            j/=5;
+        }
+    }
+    k3 = k3/2;
+    if (k3 > k5)
+    {
+        out << k5;
+    }
+    else
+    {
+        out << k3;
+    }
+    out.close();
+    return 0;
+}
